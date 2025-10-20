92cedce12ae49ec4cf4d674b3a12f5579973fbb9
Rob Clark <rob.clark@oss.qualcomm.com>
2025-10-20 12:00:49 -0700

    freedreno/registers: Name HYSTERESIS regs
    It looks like we'll need to program them for gen8.
    
    Signed-off-by: Rob Clark <rob.clark@oss.qualcomm.com>

--- src/freedreno/vulkan/tu_cmd_buffer.cc
+++ src/freedreno/vulkan/tu_cmd_buffer.cc
@@ -264,11 +264,11 @@ tu_emit_rt_workaround(struct tu_cmd_buffer *cmd, struct tu_cs *cs)
    tu_cs_emit_pkt7(cs, CP_SET_MARKER, 1);
    tu_cs_emit(cs, A6XX_CP_SET_MARKER_0_RT_WA_START);
 
-   tu_cs_emit_regs(cs, A7XX_SP_CS_UNKNOWN_A9BE(.dword = 0x10000));
-   tu_cs_emit_regs(cs, A7XX_SP_PS_UNKNOWN_A9AB(.dword = 0x10000));
+   tu_cs_emit_regs(cs, SP_CS_HYSTERESIS(A7XX, .dword = 0x10000));
+   tu_cs_emit_regs(cs, SP_PS_HYSTERESIS(A7XX, .dword = 0x10000));
    tu_emit_event_write<A7XX>(cmd, cs, FD_DUMMY_EVENT);
-   tu_cs_emit_regs(cs, A7XX_SP_CS_UNKNOWN_A9BE(.dword = 0));
-   tu_cs_emit_regs(cs, A7XX_SP_PS_UNKNOWN_A9AB(.dword = 0));
+   tu_cs_emit_regs(cs, SP_CS_HYSTERESIS(A7XX, .dword = 0));
+   tu_cs_emit_regs(cs, SP_PS_HYSTERESIS(A7XX, .dword = 0));
    tu_emit_event_write<A7XX>(cmd, cs, FD_DUMMY_EVENT);
    tu_emit_event_write<A7XX>(cmd, cs, FD_DUMMY_EVENT);
    tu_emit_event_write<A7XX>(cmd, cs, FD_DUMMY_EVENT);
